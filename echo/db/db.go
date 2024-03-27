06ec0f6eccbda4ff72aef14f941117a2c41574e4
hiroki <hiro@hirokinoMacBook-Pro.local>
2024-03-27 20:33:34 +0900

    [ADD]dbを追加

---
+++ echo/db/db.go
@@ -0,0 +1,24 @@
+package db
+
+import (
+	"app/config"
+	"database/sql"
+	"fmt"
+)
+
+func NewDB(cfg *config.Config) (*sql.DB, error) {
+	dsn := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable",
+		cfg.DBHost, cfg.DBPort, cfg.DBUser, cfg.DBPassword, cfg.DBName)
+
+	db, err := sql.Open("postgres", dsn)
+	if err != nil {
+		return nil, err
+	}
+
+	err = db.Ping()
+	if err != nil {
+		return nil, err
+	}
+
+	return db, nil
+}
