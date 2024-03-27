ce3bf520fe2e40bf6d03e6ce15f7d7d449d19ea3
hiroki <hiro@hirokinoMacBook-Pro.local>
2024-03-27 20:34:04 +0900

    [ADD]configを追加

---
+++ echo/config/config.go
@@ -0,0 +1,21 @@
+package config
+
+import "os"
+
+type Config struct {
+	DBHost     string
+	DBUser     string
+	DBPassword string
+	DBName     string
+	DBPort     string
+}
+
+func NewConfig() *Config {
+	return &Config{
+		DBHost:     os.Getenv("DB_HOST"),
+		DBUser:     os.Getenv("DB_USER"),
+		DBPassword: os.Getenv("DB_PASSWORD"),
+		DBName:     os.Getenv("DB_NAME"),
+		DBPort:     os.Getenv("DB_PORT"),
+	}
+}
