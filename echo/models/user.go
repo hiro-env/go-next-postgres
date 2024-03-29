0c2e669f5857556dce56307fd42a06ed828c50e7
hiroki <hiro@hirokinoMacBook-Pro.local>
2024-03-29 09:57:21 +0900

    [MOD]json表記を削除

--- echo/models/user.go
+++ echo/models/user.go
@@ -3,12 +3,12 @@ package models
 import "time"
 
 type User struct {
-	ID        int64     `json:"id" db:"id"`
-	Username  string    `json:"userName" db:"username"`
-	Nickname  string    `json:"nickname" db:"nickname"`
-	Image     []byte    `json:"image" db:"image"`
-	CreatedBy string    `json:"createdBy" db:"created_by"`
-	CreatedAt time.Time `json:"createdAt" db:"created_at"`
-	UpdatedBy string    `json:"updatedBy" db:"updated_by"`
-	UpdatedAt time.Time `json:"updatedAt" db:"updated_at"`
+	ID        int64     `db:"id"`
+	Username  string    `db:"username"`
+	Nickname  string    `db:"nickname"`
+	Image     []byte    `db:"image"`
+	CreatedBy string    `db:"created_by"`
+	CreatedAt time.Time `db:"created_at"`
+	UpdatedBy string    `db:"updated_by"`
+	UpdatedAt time.Time `db:"updated_at"`
 }
