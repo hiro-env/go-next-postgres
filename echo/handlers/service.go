899e6a56f474c31ef6fd1ec161d3773d60157095
hiroki <hiro@hirokinoMacBook-Pro.local>
2024-04-02 19:01:15 +0900

    [MOD]サービスインターフェースの追加

--- echo/handlers/service.go
+++ echo/handlers/service.go
@@ -1,12 +1,23 @@
 package handlers
 
-import "app/models"
+import (
+	"app/models"
+	"github.com/labstack/echo/v4"
+	"net/http"
+)
 
 type UserService interface {
 	SearchUsers(nickname string) ([]models.User, error)
+	GetUserInfo(int64) (*models.UserInfo, error)
+	UpdateUserInfo(int64, *models.UpdateUserInfo) error
 }
 
 type AccountService interface {
-	CreateUser(request *models.UserRegisterRequest) (int64, error)
+	Verify(echo.Context) bool
+	CreateUser(*models.UserAuthRequest) (int64, error)
+	Login(*models.UserAuthRequest) (int64, error)
 	GetAllUsernames() ([]string, error)
+	CreateJWT(int64) (string, error)
+	CreateBeingDeletedCookie() *http.Cookie
+	DeleteUser(int64) error
 }
