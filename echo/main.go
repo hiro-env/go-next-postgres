a77c63955d23e51916fe43413ec910bd5f87f0ed
hiroki <hiro@hirokinoMacBook-Pro.local>
2024-04-02 17:55:58 +0900

    [FIX]main.goのCORSのmiddlewareにてCookiesを受け入れられるよう修正

--- echo/main.go
+++ echo/main.go
@@ -4,11 +4,12 @@ import (
 	"app/config"
 	"app/db"
 	"app/handlers"
+	"app/middleware"
 	"app/repositories"
 	"app/routes"
 	"app/services"
 	"github.com/labstack/echo/v4"
-	"github.com/labstack/echo/v4/middleware"
+	echoMiddleware "github.com/labstack/echo/v4/middleware"
 	_ "github.com/lib/pq"
 )
 
@@ -28,13 +29,15 @@ func main() {
 	accountService := services.NewAccountService(accountRepo)
 	accountHandler := handlers.NewAccountHandler(accountService)
 
-	e := echo.New() // Echoのインスタンスを作成
+	e := echo.New()
 
-	e.Use(middleware.CORSWithConfig(middleware.CORSConfig{
-		AllowOrigins: []string{"http://localhost:3000"}, // すべてのオリジンを許可
-		AllowMethods: []string{echo.GET, echo.PUT, echo.POST, echo.DELETE},
+	e.Use(echoMiddleware.CORSWithConfig(echoMiddleware.CORSConfig{
+		AllowOrigins:     []string{"http://localhost:3000"},
+		AllowMethods:     []string{echo.GET, echo.PUT, echo.POST, echo.DELETE},
+		AllowCredentials: true,
 	}))
+	e.Use(middleware.JWTMiddleware)
 
 	routes.Init(e, userHandler, accountHandler)
-	e.Logger.Fatal(e.Start(":8080")) // 8080ポートでサーバを起動
+	e.Logger.Fatal(e.Start(":8080"))
 }
