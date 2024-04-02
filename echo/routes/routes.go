e93675724dc6bd48c7e053be0105134622072c7d
hiroki <hiro@hirokinoMacBook-Pro.local>
2024-04-02 17:58:46 +0900

    [MOD]認証機能のルートを追加

--- echo/routes/routes.go
+++ echo/routes/routes.go
@@ -5,6 +5,13 @@ import (
 	"github.com/labstack/echo/v4"
 )
 
-func Init(e *echo.Echo, userHandler *handlers.UserHandler) {
-	e.GET("/users", userHandler.GetUsers)
+func Init(e *echo.Echo, userHandler *handlers.UserHandler, accountHandler *handlers.AccountHandler) {
+	e.GET("/mypage", userHandler.GetMyPageInfo)
+	e.POST("/users", userHandler.GetUsers)
+	e.POST("/updateuser", userHandler.UpdateMypage)
+	e.GET("/verify", accountHandler.Verify)
+	e.GET("/logout", accountHandler.HandleLogout)
+	e.GET("/delete", accountHandler.HandleDelete)
+	e.POST("/register", accountHandler.Register)
+	e.POST("/login", accountHandler.HandleLogin)
 }
